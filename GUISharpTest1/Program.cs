b12f8c3d6c59d072c59c5821df94b6e0b793731f
Aliwoto <aminnimaj@gmail.com>
2021-06-28 09:44:12 +0000

    Switching to GUISharp
    Signed-off-by: Aliwoto <aminnimaj@gmail.com>

--- GUISharpTest1/Program.cs
+++ GUISharpTest1/Program.cs
@@ -1,13 +1,14 @@
 using System;
 
-namespace GUIVoidTest1
+namespace GUISharpTest1
 {
 	public class Program
 	{
 		[STAThread]
 		private static void Main()
 		{
-			GUIVoid.Client.GUIClient test = new();
+			GUISharp.Client.GUIClient test = new();
+			test.ChangeLocation(0, 0);
 			test.Start();
 		}
 	}
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# GUIVoidTest1
+# GUISharpTest1
 --------------------
-A simple project to show you how to work with GUIVoid.
+A simple project to show you how to work with GUISharp.
 This repo is still incomplete.
 It will become competed in the future (Soon enough).
